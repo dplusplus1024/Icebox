af3b9e875a7e3a611e40fc4fa36646d0b00d0626
D++ <82842780+dplusplus1024@users.noreply.github.com>
2023-01-29 20:00:32 -0600

    Add files via upload

--- my-footer.js
+++ my-footer.js
@@ -1,6 +1,6 @@
 document.write('\
 <!--img src=images/logo-small.jpg><br-->\
 <!--i style="font-size:110%"><a href=https://discord.gg/bKvdZS8DKa>Join the <b><u>D++ Discord</b></u> for future workshops!<br></a></i-->\
-©<span id="hide-on-desktop">&nbsp;</span>2023 &nbsp;<span id="hide-on-desktop">&nbsp;</span>D++ <span id="hide-on-desktop"> &nbsp; </span> <a href=https://dplusplus.me>http://dplusplus.me</a><span id="hide-on-mobile">&nbsp;&nbsp;&nbsp;</span><div id="hide-on-desktop"></div><a href=https://twitter.com/D_plus__plus><img src="Twitter.jpg" width=14> d_plus__plus</a>&nbsp;&nbsp;&nbsp;<a href=https://www.instagram.com/d_plus_plus/><img src="Instagram.jpg" width=14> d_plus_plus</a>\
+©<span class="hide-on-desktop">&nbsp;</span>2023 &nbsp;<span class="hide-on-desktop">&nbsp;</span>D++ <span class="hide-on-desktop"> &nbsp; </span> <a href=https://dplusplus.me>http://dplusplus.me</a><span class="hide-on-mobile">&nbsp;&nbsp;&nbsp;</span><div class="hide-on-desktop"></div><a href=https://twitter.com/D_plus__plus><img src="Twitter.jpg" width=14> d_plus__plus</a>&nbsp;&nbsp;&nbsp;<a href=https://www.instagram.com/d_plus_plus/><img src="Instagram.jpg" width=14> d_plus_plus</a>\
 \
 ');
